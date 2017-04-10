8634d58aadf918c74e42d4bac0980798464737e2
Jeanmarie <jeanmarie@dynamicalperspectives.com>
2017-04-10 08:41:53 -0400

    initial test files

---
+++ demo/userauth/config.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// var config = {
+//   apiKey: "YOUR_API_KEY",
+//   authDomain: "your-app.firebaseapp.com",
+//   databaseURL: "https://your-app.firebaseio.com",
+//   storageBucket: "your-app.appspot.com",
+// };
+// firebase.initializeApp(config);
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCCa-uKCvNVAXrN_kB15Rmz2EWuE1rc4Qc",
+    authDomain: "hear-and-now-7068c.firebaseapp.com",
+    databaseURL: "https://hear-and-now-7068c.firebaseio.com",
+    projectId: "hear-and-now-7068c",
+    storageBucket: "hear-and-now-7068c.appspot.com",
+    messagingSenderId: "932405017293"
+  };
+  firebase.initializeApp(config);
+</script>
